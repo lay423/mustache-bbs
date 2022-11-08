8669d84dfc7b5d955caa3a44c525522aa0df4030
lay423 <junpang486@gmail.com>
2022-11-08 11:07:17 +0900

    repository 추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -2,7 +2,9 @@ package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 public class ArticleController {
 
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping("/new")
     public String newArticleForm() {
         return "articles/new";
@@ -22,6 +30,7 @@ public class ArticleController {
     public String createArticle(ArticleDto form) {
         log.info(form.toString());
         Article article = form.toEntity();
+        articleRepository.save(article);
         return "";
     }
 }
